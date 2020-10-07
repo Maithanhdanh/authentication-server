d4dbaa71c781cdbf2b05651fddc88c5ac1c3f45e
Mai Thanh Danh <mtd.maithanhdanh@gmail.com>
2020-10-07 11:45:44 +0700

    [UPDATE] ecosystem config for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,41 +1,41 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  },{
-name: 'Authenication server',
-autorestart: true,
-watch: true,
-env: {
-      NODE_ENV: 'production'
-    },
-},
-],
+    apps: [
+        {
+            name: "app-name",
+            script: "./index.js",
+            cwd: __dirname, // path-to-project
+            instances: 1, // default 1
+            autorestart: true,
+            exec_mode: "cluster", // allow scale up app
+            watch: true,
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
 
-  deploy : {
-    production : {
-      user : 'ubuntu',
-      host : '13.212.81.27',
-      ref  : 'origin/master',
-      repo : 'https://github.com/Maithanhdanh/authentication-server.git',
-      path : '/authentication-server',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-env: {
-                NODE_ENV: 'production',
-PORT: '5000',
-SECRET: 'Vuslacontrai42109',
-SESSION_SECRET: 'Dantisdeptrai',
-ACCESS_TOKEN: 'Vuslacontrai42109',
-REFRESH_TOKEN: 'Vusdeophailacongai42109',
-AUDIENCE: 'http://authenticationServer',
-ISSUER: 'Dantis',
-PORT: '5000'
-            }
-    }
-  }
-};
+    deploy: {
+        production: {
+            user: "ubuntu",
+            host: "13.212.81.27",
+            ref: "origin/master",
+            repo: "https://github.com/Maithanhdanh/authentication-server.git",
+            path: "/authentication-server",
+            "pre-deploy-local": "",
+            "post-deploy":
+                "npm install && pm2 reload ecosystem.config.js --env production",
+            "pre-setup": "",
+            env: {
+                NODE_ENV: "production",
+                PORT: "5000",
+                SECRET: "Vuslacontrai42109",
+                SESSION_SECRET: "Dantisdeptrai",
+                ACCESS_TOKEN: "Vuslacontrai42109",
+                REFRESH_TOKEN: "Vusdeophailacongai42109",
+                AUDIENCE: "http://authenticationServer",
+                ISSUER: "Dantis",
+                PORT: "5000",
+            },
+        },
+    },
+}

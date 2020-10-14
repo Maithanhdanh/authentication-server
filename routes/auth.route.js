16f1d7617b1628ab87ee2185e7d4fdbb243fcde8
Mai Thanh Danh <mtd.maithanhdanh@gmail.com>
2020-10-14 16:15:36 +0700

    [UPDATE] commit before merge Unit test

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,20 +5,6 @@ const controller = require("../controllers/auth.controller")
 const { validate } = require("../validation/auth.validation")
 const passport = require("passport")
 
-// const posts = [
-//     {
-//         username: "Dantis",
-//         title: "post 1",
-//     },
-//     {
-//         username: "Vus",
-//         title: "post 2",
-//     },
-// ]
-// router.get("/posts", authenticateToken, (req, res) => {
-//     res.json(posts.filter((post) => post.username === req.user.sub))
-// })
-
 router.route("/login").post(
     passport.authenticate("local"),
     controller.Login
